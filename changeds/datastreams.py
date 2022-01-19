56db5f33d22d878ac0d9a8d546679ad180ba5c32
Florian Kalinke <flops.ka@gmail.com>
2022-01-19 09:46:17 +0100

    add cifar10, cifar100

--- changeds/datastreams.py
+++ changeds/datastreams.py
@@ -5,7 +5,7 @@ from changeds.abstract import ChangeStream
 
 
 class SortedMNIST(ChangeStream):
-    def __init__(self):
+    def __init__(self, preprocess = None):
         (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
         x_train = np.reshape(x_train, newshape=(len(x_train), x_train.shape[1] * x_train.shape[2]))
         x_test = np.reshape(x_test, newshape=(len(x_test), x_test.shape[1] * x_test.shape[2]))
@@ -14,13 +14,59 @@ class SortedMNIST(ChangeStream):
         sorted_indices = np.argsort(y)
         x = x[sorted_indices]
         y = y[sorted_indices]
+        if preprocess:
+            x = preprocess(x)
         self._change_points = np.diff(y, prepend=y[0]).astype(bool)
         super(SortedMNIST, self).__init__(data=x, y=y)
 
     def _is_change(self) -> bool:
         return self._change_points[self.sample_idx]
 
+def rgb2gray(rgb):
+    return np.dot(rgb[:,:,:3], [0.299, 0.587, 0.114])
 
+class SortedCIFAR10(ChangeStream):
+    def __init__(self, preprocess = None):
+        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+        x_train = x_train.dot([0.299, 0.587, 0.114])
+        x_test = x_test.dot([0.299, 0.587, 0.114])
+        x_train = np.reshape(x_train, newshape=(len(x_train), x_train.shape[1] * x_train.shape[2]))
+        x_test = np.reshape(x_test, newshape=(len(x_test), x_test.shape[1] * x_test.shape[2]))
+        x = np.vstack([x_train, x_test])
+        y = np.hstack([y_train.reshape(-1), y_test.reshape(-1)])
+        sorted_indices = np.argsort(y)
+        x = x[sorted_indices]
+        y = y[sorted_indices]
+        
+        if preprocess:
+            x = preprocess(x)
+        self._change_points = np.diff(y, prepend=y[0]).astype(bool)
+        super(SortedCIFAR10, self).__init__(data=x, y=y)
+
+    def _is_change(self) -> bool:
+        return self._change_points[self.sample_idx]
+
+class SortedCIFAR100(ChangeStream):
+    def __init__(self, preprocess = None):
+        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+        x_train = x_train.dot([0.299, 0.587, 0.114])
+        x_test = x_test.dot([0.299, 0.587, 0.114])
+        x_train = np.reshape(x_train, newshape=(len(x_train), x_train.shape[1] * x_train.shape[2]))
+        x_test = np.reshape(x_test, newshape=(len(x_test), x_test.shape[1] * x_test.shape[2]))
+        x = np.vstack([x_train, x_test])
+        y = np.hstack([y_train.reshape(-1), y_test.reshape(-1)])
+        sorted_indices = np.argsort(y)
+        x = x[sorted_indices]
+        y = y[sorted_indices]
+
+        if preprocess:
+            x = preprocess(x)
+        self._change_points = np.diff(y, prepend=y[0]).astype(bool)
+        super(SortedCIFAR100, self).__init__(data=x, y=y)
+
+    def _is_change(self) -> bool:
+        return self._change_points[self.sample_idx]
+    
 if __name__ == '__main__':
     mnist = SortedMNIST()
     while mnist.has_more_samples():
