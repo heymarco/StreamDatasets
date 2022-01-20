87171d82b7a716faf7f7443c5954c02255083e08
Marco Heyden <marco.heyden@kit.edu>
2022-01-20 12:12:26 +0100

    Added fashion mnist dataset

--- changeds/datastreams.py
+++ changeds/datastreams.py
@@ -5,7 +5,7 @@ from changeds.abstract import ChangeStream
 
 
 class SortedMNIST(ChangeStream):
-    def __init__(self, preprocess = None):
+    def __init__(self, preprocess=None):
         (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
         x_train = np.reshape(x_train, newshape=(len(x_train), x_train.shape[1] * x_train.shape[2]))
         x_test = np.reshape(x_test, newshape=(len(x_test), x_test.shape[1] * x_test.shape[2]))
@@ -22,11 +22,32 @@ class SortedMNIST(ChangeStream):
     def _is_change(self) -> bool:
         return self._change_points[self.sample_idx]
 
+
+class SortedFashionMNIST(ChangeStream):
+    def __init__(self, preprocess=None):
+        (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+        x_train = np.reshape(x_train, newshape=(len(x_train), x_train.shape[1] * x_train.shape[2]))
+        x_test = np.reshape(x_test, newshape=(len(x_test), x_test.shape[1] * x_test.shape[2]))
+        x = np.vstack([x_train, x_test])
+        y = np.hstack([y_train, y_test])
+        sorted_indices = np.argsort(y)
+        x = x[sorted_indices]
+        y = y[sorted_indices]
+        if preprocess:
+            x = preprocess(x)
+        self._change_points = np.diff(y, prepend=y[0]).astype(bool)
+        super(SortedFashionMNIST, self).__init__(data=x, y=y)
+
+    def _is_change(self) -> bool:
+        return self._change_points[self.sample_idx]
+
+
 def rgb2gray(rgb):
-    return np.dot(rgb[:,:,:3], [0.299, 0.587, 0.114])
+    return np.dot(rgb[:, :, :3], [0.299, 0.587, 0.114])
+
 
 class SortedCIFAR10(ChangeStream):
-    def __init__(self, preprocess = None):
+    def __init__(self, preprocess=None):
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
         x_train = x_train.dot([0.299, 0.587, 0.114])
         x_test = x_test.dot([0.299, 0.587, 0.114])
@@ -37,7 +58,7 @@ class SortedCIFAR10(ChangeStream):
         sorted_indices = np.argsort(y)
         x = x[sorted_indices]
         y = y[sorted_indices]
-        
+
         if preprocess:
             x = preprocess(x)
         self._change_points = np.diff(y, prepend=y[0]).astype(bool)
@@ -46,8 +67,9 @@ class SortedCIFAR10(ChangeStream):
     def _is_change(self) -> bool:
         return self._change_points[self.sample_idx]
 
+
 class SortedCIFAR100(ChangeStream):
-    def __init__(self, preprocess = None):
+    def __init__(self, preprocess=None):
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
         x_train = x_train.dot([0.299, 0.587, 0.114])
         x_test = x_test.dot([0.299, 0.587, 0.114])
@@ -66,10 +88,11 @@ class SortedCIFAR100(ChangeStream):
 
     def _is_change(self) -> bool:
         return self._change_points[self.sample_idx]
-    
+
+
 if __name__ == '__main__':
-    mnist = SortedMNIST()
-    while mnist.has_more_samples():
-        x, y, is_change = mnist.next_sample()
+    stream = SortedFashionMNIST()
+    while stream.has_more_samples():
+        x, y, is_change = stream.next_sample()
         if is_change:
-            print("Change at index {}".format(mnist.sample_idx))
+            print("Change at index {}".format(stream.sample_idx))
