e9655e2704c4d0d89fca14070ec43be3f487499a
Marco Heyden <marco.heyden@kit.edu>
2022-02-11 11:35:32 +0100

    added classification data sets for prequential evaluation

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-from distutils.command import build
-from os import path
-from subprocess import check_call
-
 from setuptools import setup
 
 
@@ -20,14 +16,6 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.8']
 
 
-class BuildWithSubmodules(build):
-    def run(self):
-        if path.exists('.git'):
-            check_call(['git', 'submodule', 'init'])
-            check_call(['git', 'submodule', 'update'])
-        build.run(self)
-
-
 setup(name='stream-datasets',
       description='Datasets for evaluating change detection algorithms.',
       url='https://github.com/heymarco/StreamDatasets',
@@ -41,5 +29,4 @@ setup(name='stream-datasets',
       extras_require={
         'plots':  ["matplotlib>=2.0.0", "seaborn"]
         },
-      cmdclass={"build": BuildWithSubmodules},
       )
