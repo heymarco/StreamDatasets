54205fcf71932555f8fe6d823e0a8b79ecc44c8c
Marco Heyden <marco.heyden@kit.edu>
2022-02-25 15:30:20 +0100

    Added severity to synthetic data sets

--- changeds/__init__.py
+++ changeds/__init__.py
@@ -2,4 +2,4 @@ from .datastreams.gradual import *
 from .datastreams.abrupt import *
 from .datastreams.synthetic import *
 from .datastreams.classification import *
-from .abstract import ChangeStream, RegionalChangeStream, GradualChangeStream
+from .abstract import ChangeStream, RegionalChangeStream, GradualChangeStream, QuantifiedSeverity
