690f1c5e03f7a704f4fb129e71f93389cc4de12d
Marco Heyden <marco.heyden@kit.edu>
2022-02-25 14:32:05 +0100

    Added synthetic experiments for severity and region evaluatio

--- changeds/__init__.py
+++ changeds/__init__.py
@@ -1,4 +1,5 @@
 from .datastreams.gradual import *
 from .datastreams.abrupt import *
+from .datastreams.synthetic import *
 from .datastreams.classification import *
 from .abstract import ChangeStream, RegionalChangeStream, GradualChangeStream
