97de655ecbd476855cbcb85dd85340ba29f2fa63
Marco Heyden <marco.heyden@kit.edu>
2022-03-30 14:37:28 +0200

    Added seed to synthetic data sets

--- changeds/datastreams/synthetic.py
+++ changeds/datastreams/synthetic.py
@@ -8,7 +8,8 @@ _type = "A"
 
 class Hypersphere(RandomOrderChangeStream, RegionalChangeStream, QuantifiesSeverity):
     def __init__(self, num_concepts: int = 100, n_per_concept: int = 2000,
-                 dims_drift: int = 50, dims_no_drift: int = 50, preprocess=False):
+                 dims_drift: int = 50, dims_no_drift: int = 50, preprocess=False, seed=0):
+        self.rng = np.random.default_rng(seed)
         self.dims_drift = dims_drift
         self.dims_no_drift = dims_no_drift
         self.num_concepts = num_concepts
@@ -33,17 +34,17 @@ class Hypersphere(RandomOrderChangeStream, RegionalChangeStream, QuantifiesSever
         return self._change_points[self.sample_idx]
 
     def _create_hypersphere(self):
-        data = np.random.uniform(-1, 1, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
+        data = self.rng.uniform(-1, 1, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
         return data / np.linalg.norm(data, axis=0)
 
     def _sample_severity(self):
-        return np.random.randint(low=0, high=100, size=self.num_concepts)
+        return self.rng.integers(low=0, high=100, size=self.num_concepts)
 
     def _create_data(self):
         y = np.array([i for i in self._sample_severity() for _ in range(self.n_per_concept)])
         data = self._create_hypersphere()
         data = data * np.expand_dims(y, -1)
-        uncorrelated = np.random.normal(scale=0.5 * np.average(y),
+        uncorrelated = self.rng.normal(scale=0.5 * np.average(y),
                                         size=(self.num_concepts * self.n_per_concept, self.dims_no_drift))
         data = np.concatenate([data, uncorrelated], axis=1)
         return data, y
@@ -57,13 +58,15 @@ class Hypersphere(RandomOrderChangeStream, RegionalChangeStream, QuantifiesSever
     def get_severity(self):
         new_label = self.y[self.sample_idx]
         old_label = self.y[self.sample_idx - 2]
-        severity = max(new_label, old_label) - min(new_label, old_label)
+        severity = np.abs(new_label - old_label)
         return severity
 
 
 class Gaussian(RandomOrderChangeStream, RegionalChangeStream, QuantifiesSeverity):
     def __init__(self, num_concepts: int = 100, n_per_concept: int = 2000,
-                 dims_drift: int = 50, dims_no_drift: int = 50, variance_drift: bool = False, preprocess=False):
+                 dims_drift: int = 50, dims_no_drift: int = 50, variance_drift: bool = False,
+                 preprocess=False, seed=0):
+        self.rng = np.random.default_rng(seed)
         self.num_concepts = num_concepts
         self.n_per_concept = n_per_concept
         self.dims_drift = dims_drift
@@ -89,21 +92,21 @@ class Gaussian(RandomOrderChangeStream, RegionalChangeStream, QuantifiesSeverity
             return self._create_mean_drift()
 
     def _create_mean_drift(self):
-        mean = np.random.uniform(-1.5, 1.5, size=self.num_concepts)
+        mean = self.rng.uniform(-1.5, 1.5, size=self.num_concepts)
         mean = np.array([m for m in mean for _ in range(self.n_per_concept)])
         mean = np.expand_dims(mean, -1)
-        gaussian_data = np.random.normal(loc=mean, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
-        standard_normal_data = np.random.normal(size=(self.num_concepts * self.n_per_concept, self.dims_no_drift))
+        gaussian_data = self.rng.normal(loc=mean, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
+        standard_normal_data = self.rng.normal(size=(self.num_concepts * self.n_per_concept, self.dims_no_drift))
         data = np.concatenate([gaussian_data, standard_normal_data], axis=1)
         return data, mean.flatten()
 
     def _create_variance_drift(self):
-        std = np.random.uniform(low=0, high=2, size=self.num_concepts)
+        std = self.rng.uniform(low=0, high=2, size=self.num_concepts)
         std = np.array([s for s in std for _ in range(self.n_per_concept)])
         std = np.expand_dims(std, -1)
-        drift_data = np.random.normal(scale=std, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
-        no_drift_data = np.random.normal(scale=np.mean(std),
-                                         size=(self.num_concepts * self.n_per_concept, self.dims_no_drift))
+        drift_data = self.rng.normal(scale=std, size=(self.num_concepts * self.n_per_concept, self.dims_drift))
+        no_drift_data = self.rng.normal(scale=np.mean(std),
+                                        size=(self.num_concepts * self.n_per_concept, self.dims_no_drift))
         data = np.concatenate([drift_data, no_drift_data], axis=1)
         return data, std.flatten()
 
